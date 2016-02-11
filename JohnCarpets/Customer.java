38b6572cb99b1d77c1cfca7061040f8907caa218
Chris Clark <c2000clark@gmail.com>
2016-02-11 10:04:43 +0000

    Create Customer.java

---
+++ JohnCarpets/Customer.java
@@ -0,0 +1,14 @@
+package Rubbish;
+
+public class Customer {
+	String Sfirstname;
+	String Ssurname;
+	String Stown;
+	String Sphonenum;
+public void enterDetails(String firstname, String surname, String town, String phonenum ){
+	 Sfirstname = firstname;
+	 Ssurname =  surname;
+	 Stown = town;
+	 Sphonenum = phonenum;
+}
+}
