90fa2bde279151e854bdd4b78e8e0ce3a00ea0e5
Chris Clark <c2000clark@gmail.com>
2016-02-11 10:05:04 +0000

    Create Quote.java

---
+++ JohnCarpets/Quote.java
@@ -0,0 +1,17 @@
+package Rubbish;
+
+public class Quote {
+	Customer Client;
+	int iLength;
+	int iWidth;
+	int iulayType;
+	float fcost;
+	
+public Quote(Customer Ccust, int length, int width, int Underlay){
+	Client = Ccust;
+	iLength = length;
+	iWidth = width;
+	iulayType = Underlay;
+}
+
+}
